e8a6bbf162e1f167908bf6862aef929f76484e9a
Niddam <louniddam@gmail.com>
2022-09-16 13:47:51 +0200

    [+] First commit
    [+] Expense component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import ExpenseItem from './components/ExpenseItems';
+
+const expensesArray = [
+	{
+		id: 'e1',
+		title: 'Toilet Paper',
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+	{
+		id: 'e3',
+		title: 'Car Insurance',
+		amount: 294.67,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: 'e4',
+		title: 'New Desk (Wooden)',
+		amount: 450,
+		date: new Date(2021, 5, 12),
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return (
+		<div>
+			<Expenses />
+		</div>
+	);
+}
+
+const Expenses = () => {
+
+	return(
+		<div>
+			{
+				expensesArray.map((item, index) => {
+					return(
+						<ExpenseItem key={index} expense={item} />
+					)
+				})
+			}
+		</div>
+	)
 }
 
 export default App;
